517be34e1a120800a360dba85334fa79a2fb1bfe
FrancisM4 <FrancisM4@091DS-1182-23.student.cssd.ab.ca>
2018-12-11 12:11:49 -0700

    OOP+++

--- tutorial/src/git/Invoice.java
+++ tutorial/src/git/Invoice.java
@@ -5,47 +5,71 @@ package git;
 import java.util.ArrayList;
 public class Invoice {
 	
-	private Customer theCustomer;
-	private ArrayList<LineItem> item = new ArrayList<>(); 
-	
+	private Customer theCustomer;//Variable using Customer method
+	private ArrayList<LineItem> item = new ArrayList<>(); //ArrayList to hold the items  being bought and quantity 
+
+	/**
+	 * Constructor for customer Invoice
+	 * @param theCustomer - the customer for which the invoice is meant for
+	 */
 	public Invoice(Customer theCustomer) {
-		this.theCustomer = theCustomer;
-	}
+		this.theCustomer = theCustomer;//fills theCustomer variable with parameter values 
+	}//ends Invoice
 	
-	//creates a new lineItem and adds the new lineItem to the items ArrayList
+	/**
+	 * Adds a desired product to the customer's order
+	 * @param product - product being added to the order
+	 * @param qty - amount of the product that customer wants to buy
+	 */
 	public void addToOrder(Product theProduct , int qty ) {
-		item.add(new LineItem(theProduct, qty));
+		item.add(new LineItem(theProduct, qty)); //adds an item to the ArrayList
 	}//ends addToOrder
 	
-	//Prints out all LineItems in the items ArrayList.
-	//If customer can afford the purchase also print out that order is approved or 
-	//else if they can’t afford the order show how much they are short by.
+	/**
+	 * prints out the total price of the customer's order + if they can buy it with their current funds or not
+	 */
 	public void printInvoice() {
 		System.out.println("Products");
 		for(int i = 0; i < item.size(); i++) {
 			System.out.printf(item.get(i) + " Total: $%.2f", (item.get(i).getTheProduct().getPrice() * item.get(i).getQty()));
 			System.out.println("");
-		}
+		}//end for
 		if(amountDue() <= theCustomer.getEvilFunds()) {
-			System.out.println();
-		}
-		
+			System.out.println("ORDER APPROVED");
+		}//end if
+		else {
+			double funds = theCustomer.getEvilFunds() - amountDue();//calculates the difference in funds
+			funds = funds * -1;//converts negative number to positive number
+			System.out.printf("You can't afford this order at this time. You are short by: $%.2f", funds);
+			System.out.println("");
+		}//end else
 	}//ends printInvoice
 	
-	//Returns true if customer can afford all purchase. 
+	/**
+	 * checks if the customer can afford their entire order
+	 * @param theCustomer - customer and their order
+	 * @return - true if the customer can afford the order, false otherwise
+	 */
 	public boolean canAfford(Customer theCustomer) {
-		return true;
+		if(amountDue() <= theCustomer.getEvilFunds()) {
+			return true;
+		}//end if
+		else {
+			return false;
+		}//end else
 		
 	}//end canAfford
 	
-	//returns the total amount due for this order
+	/**
+	 * calculates the total price of the customer's order
+	 * @return - total value of all items in customer's order
+	 */
 	public double amountDue() {
-		return amountDue();
+		double totalPrice = 0;
+		for(int i = 0; i < item.size(); i++) {
+			totalPrice = totalPrice + item.get(i).getTheProduct().getPrice() * item.get(i).getQty();
+		}//end for
+		return totalPrice;
 		
 	}//end amountDue
-	
-	
-	
-	
-
 }
--- tutorial/src/git/TestInvoice.java
+++ tutorial/src/git/TestInvoice.java
@@ -12,22 +12,20 @@ public class TestInvoice {
 				Customer c = new Customer("Island", "Dr. Evil", 1230.0);
 				Invoice in = new Invoice(c);
 				in.addToOrder(new Product("Flame Thower",123.80), 3);
-				in.addToOrder(new Product("Sharks", 105.2),2);
-				in.addToOrder(new Product("Lasers",50.5),20);
-				in.addToOrder(new Product("Island", 1000),1);
-				in.addToOrder(new Product("Fireworks",5),20);
-				
-				runInvoice(c,in); //runs invoice
-				
+				in.addToOrder(new Product("Sharks", 105.22),2);
+				in.addToOrder(new Product("Lasers",50.50),20);
+				in.addToOrder(new Product("Island", 1000.20),1);
+				in.addToOrder(new Product("Fireworks",5.22),20);
+				runInvoice(c,in); //runs invoice				
 				//invoice run 2
-				Customer c1 = new Customer("Beach", "Dr. Doofenshmirtz", 6000000);
+				Customer c1 = new Customer("Beach", "Dr. Doofenshmirtz", 6000000.00);
 				Invoice in1 = new Invoice(c1);
 				in1.addToOrder(new Product("Mirror", 232.10), 2);
 				in1.addToOrder(new Product("Flashlight", 309.10), 1);
 				in1.addToOrder(new Product("Magnifying Glass", 100.10), 4);
 				in1.addToOrder(new Product("Time Machine", 30000.10), 1);
 				in1.addToOrder(new Product("Tower", 1000000.10), 1);
-				
+				System.out.println();
 				runInvoice(c1, in1);
 				//invoice run 3
 				Customer c2 = new Customer("Stark Tower", "Tony Stark", 12400000000.00);
@@ -37,7 +35,7 @@ public class TestInvoice {
 				in2.addToOrder(new Product("Thrusters", 1000.00), 30);
 				in2.addToOrder(new Product("Arc Reactor", 100000000.00), 1);
 				in2.addToOrder(new Product("Paints", 100.00), 5);
-				
+				System.out.println();
 				runInvoice(c2, in2);
 				//invoice run 4
 				Customer c3 = new Customer("Wakanda", "T'Challa", 90700000000000.00);
@@ -47,7 +45,7 @@ public class TestInvoice {
 				in3.addToOrder(new Product("Vibaranium Spear", 100000.00), 1);
 				in3.addToOrder(new Product("Claws", 10000.00), 5);
 				in3.addToOrder(new Product("Techs", 100000000.00), 5);
-				
+				System.out.println();
 				runInvoice(c3, in3);
 			}//end main
 			
